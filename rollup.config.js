a2f91cf584b875d932ed9eac1132387ba4dd524e
Debjit Biswas <debjit@fastmail.fm>
2018-05-18 11:50:11 +0530

    build(all): Export as umd, es and for unpkg

--- rollup.config.js
+++ rollup.config.js
@@ -2,19 +2,52 @@
 import resolve from "rollup-plugin-node-resolve";
 import babel from "rollup-plugin-babel";
 import flow from "rollup-plugin-flow";
+import uglify from "rollup-plugin-uglify";
 
-export default {
+const year = new Date().getFullYear();
+
+const PLUGINS = [
+    resolve(),
+    flow(),
+    babel({
+        exclude: "node_modules/**" // only transpile our source code
+    })
+];
+
+const BANNER = `/*!
+* Copyright (c) ${year} Bonzai Digital Pte. Ltd., All rights reserved.
+* Licensed under BSD-3-Clause (https://github.com/bonzaico/e-vector/blob/master/LICENSE.md)
+*/
+`;
+
+export default [{
     input: "src/vector",
     output: {
         file: "dist/index.js",
         format: "umd",
-        name: "Vector"
+        name: "Vector",
+        banner: BANNER
     },
-    plugins: [
-        resolve(),
-        flow(),
-        babel({
-            exclude: "node_modules/**" // only transpile our source code
-        })
-    ]
-};
+    plugins: PLUGINS
+}, {
+    input: "src/vector",
+    output: {
+        file: "dist/index.esm.js",
+        format: "es",
+        banner: BANNER
+    },
+    plugins: PLUGINS
+}, {
+    input: "src/vector.js",
+    output: {
+        file: "dist/index.umd.min.js",
+        format: "umd",
+        name: "Vector",
+        banner: BANNER
+    },
+    plugins: PLUGINS.concat([uglify({
+        output: {
+            comments: /^!/
+        }
+    })])
+}];
